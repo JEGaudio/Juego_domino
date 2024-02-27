ac7164f760de9ea29592239bd12cf81add82382d
JEGaudio <140985074+JEGaudio@users.noreply.github.com>
2024-02-27 05:11:22 -0300

    cambios app

--- app.js
+++ app.js
@@ -30,12 +30,28 @@ function Ficha (numero, estado, seleccionado) {
     this.Seleccion = seleccionado;
 }
 
+/*
 var cartas = [new Ficha(0/6,"nada",false),new Ficha(1/6,"nada",false),new Ficha(2/6,"nada",false),new Ficha(3/6,"nada",false),new Ficha(4/6,"nada",false),new Ficha(5/6,"nada",false),new Ficha(6/6,"nada",false),new Ficha(0/5,"nada",false),new Ficha(1/5,"nada",false),new Ficha(2/5,"nada",false),new Ficha(3/5,"nada",false),new Ficha(4/5,"nada",false),new Ficha(5/5,"nada",false),new Ficha(0/4,"nada",false),new Ficha(1/4,"nada",false),new Ficha(2/4,"nada",false),new Ficha(3/4,"nada",false),new Ficha(4/4,"nada",false),new Ficha(0/3,"nada",false),new Ficha(1/3,"nada",false),new Ficha(2/3,"nada",false),new Ficha(3/3,"nada",false),new Ficha(0/2,"nada",false),new Ficha(1/2,"nada",false),new Ficha(2/2,"nada",false),new Ficha(0/1,"nada",false),new Ficha(1/1,"nada",false),new Ficha(0/0,"nada",false)];
+*/
+
+var cartas = [
+    [new Ficha(0/6,"nada",false),new Ficha(1/6,"nada",false),new Ficha(2/6,"nada",false),new Ficha(3/6,"nada",false),new Ficha(4/6,"nada",false),new Ficha(5/6,"nada",false),new Ficha(6/6,"nada",false)] 
+    [new Ficha(0/5,"nada",false),new Ficha(1/5,"nada",false),new Ficha(2/5,"nada",false),new Ficha(3/5,"nada",false),new Ficha(4/5,"nada",false),new Ficha(5/5,"nada",false)]
+    [new Ficha(0/4,"nada",false),new Ficha(1/4,"nada",false),new Ficha(2/4,"nada",false),new Ficha(3/4,"nada",false),new Ficha(4/4,"nada",false)]
+    [new Ficha(0/3,"nada",false),new Ficha(1/3,"nada",false),new Ficha(2/3,"nada",false),new Ficha(3/3,"nada",false)]
+    [new Ficha(0/2,"nada",false),new Ficha(1/2,"nada",false),new Ficha(2/2,"nada",false)]
+    [new Ficha(0/1,"nada",false),new Ficha(1/1,"nada",false)]
+    [new Ficha(0/0,"nada",false)]
+];
+
 
 const tbody = document.querySelector('#posicion_fichas tbody');
 tbody.addEventListener('click', function (e) {
   const cell = e.target.closest('td');
   if (!cell) {return;} // Quit, not clicked on a cell
-  const row = cell.parentElement;
-  console.log(cell.innerHTML, row.rowIndex, cell.cellIndex);
+  const row = cell.parentElement;   //row.rowIndex nos da la fila, lo cual sería segunda posicion del array [][x]
+  console.log(cell.innerHTML, row.rowIndex, cell.cellIndex);  //cell.cellIndex nos da la columna, lo cual sería el primer dato del array [x][]
+
+  cartas[cell.cellIndex][row.rowIndex].Seleccion = true;
+  console.log(cartas.Seleccion);
 });
